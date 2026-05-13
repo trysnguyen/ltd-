166c12f7ce7130c3d02991122c291e5d78b40c30
trysnguyen <122746846+trysnguyen@users.noreply.github.com>
2026-05-13 08:12:46 +0700

    Add files via upload

---
+++ lab9/app/src/main/java/com/example/lab9/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.lab9
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.lab9.ui.BluromaticScreen
+import com.example.lab9.ui.theme.BluromaticTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BluromaticTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    BluromaticScreen()
+                }
+            }
+        }
+    }
+}
